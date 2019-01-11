5a84e1194e8b5a95e0ce5497d4a71b02c4f305b2
Bruno Felipe <brunofelipe.elias@hotmail.com>
2019-01-11 12:53:33 -0300

    Testando classe de domino - Diretor com Postaman

---
+++ src/main/java/com/brunofelipe/sistemaescolar/domain/Diretor.java
@@ -0,0 +1,99 @@
+package com.brunofelipe.sistemaescolar.domain;
+
+import java.io.Serializable;
+
+public class Diretor implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private String cellPhone;
+	private Integer age;
+	private Float numberRegistration;
+	
+	public Diretor() {
+		
+	}
+
+	public Diretor(Integer id, String name, String cellPhone, Integer age, Float numberRegistration) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cellPhone = cellPhone;
+		this.age = age;
+		this.numberRegistration = numberRegistration;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Float getNumberRegistration() {
+		return numberRegistration;
+	}
+
+	public void setNumberRegistration(Float numberRegistration) {
+		this.numberRegistration = numberRegistration;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((numberRegistration == null) ? 0 : numberRegistration.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Diretor other = (Diretor) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (numberRegistration == null) {
+			if (other.numberRegistration != null)
+				return false;
+		} else if (!numberRegistration.equals(other.numberRegistration))
+			return false;
+		return true;
+	}
+
+	
+}
--- src/main/java/com/brunofelipe/sistemaescolar/resources/DiretorResoucer.java
+++ src/main/java/com/brunofelipe/sistemaescolar/resources/DiretorResoucer.java
@@ -1,16 +1,28 @@
 package com.brunofelipe.sistemaescolar.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brunofelipe.sistemaescolar.domain.Diretor;
 
 @RestController
 @RequestMapping(value="/diretores") //boas praticas de programação
 public class DiretorResoucer {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando bem!";
+	public List<Diretor> listar() {
+		
+		Diretor drt = new Diretor(1,"Diretor de Monitoria", null, null, null);
+		Diretor drtGeral = new Diretor(2, "Diretor Geral", null, null, null);
+	
+		List<Diretor> listadeDiretores = new ArrayList<>();
+		listadeDiretores.add(drt);
+		listadeDiretores.add(drtGeral);
+		return listadeDiretores;
 	}
 
 }
